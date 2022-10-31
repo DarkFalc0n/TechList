c536b65a8fe77277445370c17934dff44fb9b2da
DarkFalc0n <unfezant211@gmail.com>
2022-10-31 23:17:42 +0530

    changed form ui

--- src/frontend/src/pages/Login.js
+++ src/frontend/src/pages/Login.js
@@ -8,9 +8,9 @@ const Login = () => {
                     Login
                 </div>
                 <form className="flex flex-col mt-12">
-                    <label className="text-slate-600 m-1" for="email">Email</label>
+                    <label className="text-slate-600 m-1 mb-0" for="email">Email</label>
                     <input className="h-8 m-1 mb-4 px-4" type="text" id="email" name="email" />
-                    <label className="text-slate-600 m-1" for="pass">Password</label>
+                    <label className="text-slate-600 m-1 mb-0" for="pass">Password</label>
                     <input className="h-8 m-1 mb-4 px-4" type="password" id="pass" name="pass" />
                     <button className="text-white bg-slate-600 m-auto mt-10 px-4 py-2 text-xl">Submit</button>
                 </form>
--- src/frontend/src/pages/Signup.js
+++ src/frontend/src/pages/Signup.js
@@ -8,13 +8,13 @@ const Signup = () => {
                     Sign Up
                 </div>
                 <form className="flex flex-col mt-12">
-                    <label className="text-slate-600 m-1" for="Username">Username</label>
+                    <label className="text-slate-600 m-1 mb-0" for="Username">Username</label>
                     <input className="h-8 m-1 mb-4 px-4" type="text" id="fname" name="fname" />
-                    <label className="text-slate-600 m-1" for="email">Email</label>
+                    <label className="text-slate-600 m-1 mb-0" for="email">Email</label>
                     <input className="h-8 m-1 mb-4 px-4" type="text" id="email" name="email" />
-                    <label className="text-slate-600 m-1" for="pass">Password</label>
+                    <label className="text-slate-600 m-1 mb-0" for="pass">Password</label>
                     <input className="h-8 m-1 mb-4 px-4" type="password" id="pass" name="pass" />
-                    <label className="text-slate-600 m-1" for="pass">Confirm Password</label>
+                    <label className="text-slate-600 m-1 mb-0" for="pass">Confirm Password</label>
                     <input className="h-8 m-1 mb-4 px-4" type="password" id="cpass" name="cpass" />
                     <button className="text-white bg-slate-600 m-auto mt-10 px-4 py-2 text-xl">Submit</button>
                 </form>
